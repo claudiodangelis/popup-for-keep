0519fd64d0bcf33dad2ba053544a845d9eda8694
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-11-15 20:36:04 +0100

    The most is done

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,42 @@
+// We use this to prevent the "Refused to display document because display
+// forbidden by X-Frame-Options" issue
 chrome.webRequest.onHeadersReceived.addListener(
-    function(info) {
-        var headers = info.responseHeaders;
+    function(req) {
+        var headers = req.responseHeaders;
         for (var i=headers.length-1; i>=0; --i) {
             var header = headers[i].name.toLowerCase();
             if (header == 'x-frame-options' || header == 'frame-options') {
-                headers.splice(i, 1); // Remove header
+                headers.splice(i, 1);
             }
         }
         return {responseHeaders: headers};
     },
     {
-        urls: [ '*://*/*' ], // Pattern to match all http(s) pages
+        urls: [ '*://*/*' ],
         types: [ 'sub_frame' ]
     },
     ['blocking', 'responseHeaders']
 );
+
+
+/* Top level objects */
+// iframe that will wrap Keep
+var keepFrame = document.querySelector("#keep-wrapper");
+// Spinner
+var spinner = document.querySelector("#spinner-wrapper");
+
+// We handle the frame here
+// When it's loaded:
+keepFrame.onload = function () {
+    // Hide the spinner
+    spinner.style.visibility = "hidden";
+    spinner.style.display = "none";
+
+    // Show the iframe
+    keepFrame.style.visibility = "visible";
+    keepFrame.style.display = "inline";
+};
+
+keepFrame.width = 500;
+keepFrame.height = 500;
+keepFrame.src = "https://keep.google.com/keep";
