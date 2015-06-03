9957838b6d098194071b8ca2af8970929e74027b
Marcos Benatti Lauer <benatti77@gmail.com>
2015-06-03 00:28:58 -0300

    remove fontawesome styles and google analytics tracking code for fast extension loading

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 // Google Analytics tracking code
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-60445871-1']);
-_gaq.push(['_trackPageview']);
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+//var _gaq = _gaq || [];
+//_gaq.push(['_setAccount', 'UA-60445871-1']);
+//_gaq.push(['_trackPageview']);
+//(function() {
+//  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+//  ga.src = 'https://ssl.google-analytics.com/ga.js';
+//  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+//})();
 /* Top level objects */
 var GOOGLE_ACCOUNT_URL = "https://accounts.google.com/ServiceLogin";
 // iframe that will wrap Keep
