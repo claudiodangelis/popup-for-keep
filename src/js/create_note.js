8425ae21d54ab6bbe61b1eaa841df01780c74381
Claudio d'Angelis <claudiodangelis@gmail.com>
2016-04-07 01:20:07 +0200

    [#18] Changing DOM selectors

--- src/js/create_note.js
+++ src/js/create_note.js
@@ -1,18 +1,26 @@
 var DOM = {
     main: 'body > div.notes-container.RfDI4d-sKfxWe > div.RfDI4d-bN97Pc > div.h1U9Be-xhiy4.qAWA2 > div.IZ65Hb-n0tgWb > div.IZ65Hb-TBnied.HLvlvd-h1U9Be > div.IZ65Hb-s2gQvd > div.notranslate.IZ65Hb-YPqjbf.h1U9Be-YPqjbf.LwH6nd',
-    textWrapper: 'body > div.notes-container.RfDI4d-sKfxWe > div.RfDI4d-bN97Pc > div.h1U9Be-xhiy4 > div.IZ65Hb-n0tgWb.IZ65Hb-QQhtn > div.IZ65Hb-TBnied.HLvlvd-h1U9Be > div.IZ65Hb-s2gQvd > div.notranslate.IZ65Hb-YPqjbf.h1U9Be-YPqjbf.LwH6nd',
+    textWrapper: 'body > div.notes-container.RfDI4d-sKfxWe > div.RfDI4d-bN97Pc.ogm-kpc > div.h1U9Be-xhiy4 > div.IZ65Hb-n0tgWb.IZ65Hb-QQhtn > div.IZ65Hb-TBnied.HLvlvd-h1U9Be > div.IZ65Hb-s2gQvd > div.notranslate.IZ65Hb-YPqjbf.h1U9Be-YPqjbf.LwH6nd',
     titleWrapper: 'body > div.notes-container.RfDI4d-sKfxWe > div.RfDI4d-bN97Pc > div.h1U9Be-xhiy4 > div.IZ65Hb-n0tgWb.IZ65Hb-QQhtn > div.IZ65Hb-TBnied.HLvlvd-h1U9Be > div.IZ65Hb-s2gQvd > div.notranslate.IZ65Hb-YPqjbf.r4nke-YPqjbf.LwH6nd',
-    title: 'body > div.notes-container.RfDI4d-sKfxWe > div.RfDI4d-bN97Pc > div.h1U9Be-xhiy4 > div.IZ65Hb-n0tgWb.IZ65Hb-QQhtn > div.IZ65Hb-TBnied.HLvlvd-h1U9Be > div.IZ65Hb-s2gQvd > div:nth-child(3)',
-    text: 'body > div.notes-container.RfDI4d-sKfxWe > div.RfDI4d-bN97Pc > div.h1U9Be-xhiy4 > div.IZ65Hb-n0tgWb.IZ65Hb-QQhtn > div.IZ65Hb-TBnied.HLvlvd-h1U9Be > div.IZ65Hb-s2gQvd > div:nth-child(5)'
+    title: 'body > div.notes-container.RfDI4d-sKfxWe > div.RfDI4d-bN97Pc.ogm-kpc > div.h1U9Be-xhiy4 > div.IZ65Hb-n0tgWb.IZ65Hb-QQhtn > div.IZ65Hb-TBnied.HLvlvd-h1U9Be > div.IZ65Hb-s2gQvd > div:nth-child(4)',
+    text: 'body > div.notes-container.RfDI4d-sKfxWe > div.RfDI4d-bN97Pc.ogm-kpc > div.h1U9Be-xhiy4 > div.IZ65Hb-n0tgWb.IZ65Hb-QQhtn > div.IZ65Hb-TBnied.HLvlvd-h1U9Be > div.IZ65Hb-s2gQvd > div:nth-child(6)'
 };
 chrome.runtime.onMessage.addListener(function (note, _, sendResponse) {
-    document.querySelector(DOM.main).click();
-    document.querySelector(DOM.textWrapper).textContent = '';
-    document.querySelector(DOM.title).textContent = note.title;
-    document.querySelector(DOM.text).textContent = note.text;
-    document.querySelector(DOM.title).focus();
-    document.querySelector(DOM.titleWrapper).textContent = '';
-    document.querySelector(DOM.title).blur();
+    var main, title, text, titleWrapper, textWrapper;
+    main = document.querySelector(DOM.main);
+    main.click();
+    textWrapper = document.querySelector(DOM.textWrapper);
+    textWrapper.textContent = '';
+    title = document.querySelector(DOM.title);
+    title.click();
+    title.textContent = note.title;
+    text = document.querySelector(DOM.text);
+    text.textContent = note.text;
+    text.click();
+    title.focus();
+    titleWrapper = document.querySelector(DOM.titleWrapper);
+    titleWrapper.textContent = '';
+    title.blur();
     sendResponse({ status: 'done' });
     history.pushState(null, null, '/keep');
 });
