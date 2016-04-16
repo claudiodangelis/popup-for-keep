22d3f05581eb516973dd0b2f0baf35d67770cb48
Claudio d'Angelis <claudiodangelis@gmail.com>
2016-04-16 15:45:30 +0200

    [#19] Making 'Add to Keep' user-wise

--- src/js/background.js
+++ src/js/background.js
@@ -1,35 +1,3 @@
-var addToKeep = function (info, tab) {
-    var title, text;
-    title = tab.title;
-    if (typeof info.selectionText === 'undefined') {
-        text = tab.url;
-    } else {
-        text = [info.selectionText, tab.url].join('\n\n');
-    }
-    chrome.tabs.create({
-        url: 'https://keep.google.com/keep?create_note'
-    }, function (target) {
-        chrome.tabs.onUpdated.addListener(function listener(id, info) {
-            if (id === target.id && info.status === 'complete') {
-                chrome.tabs.sendMessage(target.id, {
-                    title: title, text:text
-                }, {}, function (response) {
-                    if (typeof response !== 'undefined' && response.status === 'done') {
-                        chrome.tabs.onUpdated.removeListener(listener);
-                    }
-                });
-            }
-        });
-    });
-};
-// Create context menus
-['page', 'selection'].forEach(function (context) {
-    chrome.contextMenus.create({
-        'title': ['Add', context, 'to Google Keep'].join(' '),
-        'contexts': [ context ],
-        'onclick': addToKeep
-    });
-});
 // Load settings
 var settings;
 Settings.load(function (data) {
@@ -83,4 +51,37 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         sendResponse(settings);
     }
     return true;
+});
+var addToKeep = function (info, tab) {
+    var title, text;
+    title = tab.title;
+    if (typeof info.selectionText === 'undefined') {
+        text = tab.url;
+    } else {
+        text = [info.selectionText, tab.url].join('\n\n');
+    }
+    var postfix = settings.accounts.lastUsed || '0';
+    chrome.tabs.create({
+        url: 'https://keep.google.com/u/' + postfix + '/?create_note'
+    }, function (target) {
+        chrome.tabs.onUpdated.addListener(function listener(id, info) {
+            if (id === target.id && info.status === 'complete') {
+                chrome.tabs.sendMessage(target.id, {
+                    title: title, text:text
+                }, {}, function (response) {
+                    if (typeof response !== 'undefined' && response.status === 'done') {
+                        chrome.tabs.onUpdated.removeListener(listener);
+                    }
+                });
+            }
+        });
+    });
+};
+// Create context menus
+['page', 'selection'].forEach(function (context) {
+    chrome.contextMenus.create({
+        'title': ['Add', context, 'to Google Keep'].join(' '),
+        'contexts': [ context ],
+        'onclick': addToKeep
+    });
 });
