7312dfc4d168a2d01a2d35d24971f749a7226041
Claudio d'Angelis <claudiodangelis@gmail.com>
2019-10-31 10:39:56 +0100

    fix selector

--- src/ts/common/account.ts
+++ src/ts/common/account.ts
@@ -37,7 +37,7 @@ function createAccountFromFragment(html: string, index: number): Promise<Account
         // If info is null it means that the account is a Google Suite account
         if (info === null) {
             // Check if there is the element we're looking for
-            let node = infoNode.querySelector('a[aria-label*="Google Account"], a[aria-label*="Google-account"]')
+            let node = infoNode.querySelector('a.gb_B.gb_Da.gb_g')
             if (node) {
                 info = node.getAttribute('aria-label')
             }
